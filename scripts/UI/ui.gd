9ae5d337d9943da71606cc1d180da2331a1f62cb
Djoeikans <lucasrodriguesjoaquim@gmail.com>
2024-06-01 20:43:13 -0300

    toques-na-contagem-de-moedas

--- scripts/UI/ui.gd
+++ scripts/UI/ui.gd
@@ -1,16 +1,20 @@
 extends CanvasLayer
 
-@onready var animation = $animation
+@onready var animation_life = $animation_life
+@onready var gear_counter = $gear_counter
 
 func _ready():
 	pass
 
 func _process(delta):
 	if(player_data.life == 3):
-		animation.play("three_lifes")
+		animation_life.play("three_lifes")
 	elif(player_data.life == 2):
-		animation.play("two_lifes")
+		animation_life.play("two_lifes")
 	if(player_data.life == 1):
-		animation.play("one_life")
+		animation_life.play("one_life")
 	if(player_data.life == 0):
-		animation.play("no_life")
+		animation_life.play("no_life")
+	
+	#é necessário mudar o tipo de variável de inteiro para string para funcionar com o ".text"
+	gear_counter.text = var_to_str(player_data.coin)
