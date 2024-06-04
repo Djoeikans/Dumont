cf5a6bafb7d4f5865f1549bd7aea77513c641c8a
Djoeikans <lucasrodriguesjoaquim@gmail.com>
2024-06-04 17:33:02 -0300

    viva-o-chat-gpt-codigo-arrumado

--- scripts/rooms/create_rooms.gd
+++ scripts/rooms/create_rooms.gd
@@ -4,21 +4,7 @@ extends Node2D
 @onready var entrance = $entrance
 
 func _ready():
-	create_rooms()
-
-func create_rooms():
-	randomize()
-	var id = randi_range(RoomsGlobals.first_room_id, RoomsGlobals.last_room_id)
-	
-	if(RoomsGlobals.rooms_to_be_created > 1):
-		var s = load("res://scenes/sewer_rooms/room_" + str(id) + ".tscn").instantiate()
-		s.position = $exit.position
-		add_child(s)
-		RoomsGlobals.rooms_to_be_created -= 1
-	else:
-		var s = load("res://scenes/sewer_rooms/exit_room.tscn").instantiate()
-		s.position = $exit.position
-		add_child(s)
+	pass
 
 func _on_entrance_body_entered(body):
 	if(body.name == "player"):
--- scripts/rooms/initial_room.gd
+++ scripts/rooms/initial_room.gd
@@ -2,19 +2,30 @@ extends Node2D
 
 @onready var tile_map = $tile_map
 @onready var camera_2d = $player/Camera2D
+@onready var exit = $exit  # Adicione a referência ao nó de saída
 
 func _ready():
 	RoomsGlobals.rooms_to_be_created = randi_range(RoomsGlobals.min_rooms, RoomsGlobals.max_rooms)
-	penis()
+	randomize()  # Certifique-se de randomizar apenas uma vez no início
+	create_rooms()
 	camera_2d.update_camera(tile_map)
 	print(RoomsGlobals.rooms_to_be_created)
 
-func penis():
-	randomize()
-	var id = randi_range(1, RoomsGlobals.last_room_id)
+func create_rooms():
+	var current_exit_position = $exit.position
 	
-	if(RoomsGlobals.rooms_to_be_created > 1):
-		var s = load("res://scenes/sewer_rooms/room_" + str(id) + ".tscn").instantiate()
-		s.position = $exit.position
+	while RoomsGlobals.rooms_to_be_created > 0:
+		var id = randi_range(RoomsGlobals.first_room_id, RoomsGlobals.last_room_id)
+		var scene_path = "res://scenes/sewer_rooms/room_" + str(id) + ".tscn"
+		
+		if RoomsGlobals.rooms_to_be_created == 1:
+			scene_path = "res://scenes/sewer_rooms/exit_room.tscn"
+		
+		var s = load(scene_path).instantiate()
+		s.position = current_exit_position  # Posicione a nova sala na posição de saída da sala atual
 		add_child(s)
+		
+		# Atualize a posição de saída para a nova sala
+		current_exit_position = s.get_node("exit").position + s.position
+		
 		RoomsGlobals.rooms_to_be_created -= 1
